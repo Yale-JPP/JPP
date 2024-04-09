716a6962ce3bca003d8c2560973f85e63dc19e9f
PerryThePear <91431688+PerryThePear@users.noreply.github.com>
2024-04-09 00:55:12 -0400

    Audio recorder
    Preliminary audio recorder made.

--- jpp/src/App.js
+++ jpp/src/App.js
@@ -1,10 +1,97 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from '@mui/material';
+import { useState, useRef } from 'react';
 
 function App() {
+
+  const [stream, setStream] = useState();
+  const mediaRecorder = useRef();
+  const [permission, setPermission] = useState(false);
+  const [chunks, setChunks] = useState([]);
+  // const [audio, setAudio] = useState("");
+
+  const getUserPermission = async () => {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      console.log("getUserMedia supported.");
+      navigator.mediaDevices
+        .getUserMedia(
+          // constraints - only audio needed for this app
+          {
+            audio: true,
+          },
+        )
+  
+        // Success callback
+        .then((stream) => {
+          setStream(stream)
+          setPermission(true)
+          console.log(stream)
+        })
+  
+        // Error callback
+        .catch((err) => {
+          console.error(`The following getUserMedia error occurred: ${err}`);
+        });
+    } else {
+      console.log("getUserMedia not supported on your browser!");
+    }
+  }
+
+  const startRecording = () => {
+    const media = new MediaRecorder(stream);
+
+    mediaRecorder.current = media;
+
+    mediaRecorder.current.start();
+    console.log(mediaRecorder.current.state);
+    console.log("recorder started");
+    
+    let localChunks = [];
+
+    mediaRecorder.current.ondataavailable = (e) => {
+      localChunks.push(e.data);
+    };
+
+    setChunks(localChunks)
+
+  }
+
+  const stopRecording = () => {
+    mediaRecorder.current.stop();
+    console.log(mediaRecorder.current.state);
+    // console.log("recorder stopped");
+
+    mediaRecorder.current.onstop = (e) => {
+      console.log("recorder stopped");
+    
+      const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+      const audioURL = window.URL.createObjectURL(blob);
+      // setAudio(audioURL);
+      // audio.src = audioURL
+
+      document.getElementById("audio").src=audioURL;
+
+      console.log(audioURL)
+    };
+    
+  }
+
+  const recordClick = async () => {
+    if (!permission) {
+      getUserPermission();
+    }
+    else {
+      startRecording();
+    } 
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <Button variant="contained" onClick={recordClick}>Record</Button>
+      <Button variant="contained" onClick={stopRecording}>Stop</Button>
+      <audio id='audio' controls></audio>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +104,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
