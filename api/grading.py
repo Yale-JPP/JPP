184071761c84cb82fe115058cff2a8733559b3cb
kayotaki <kevin.im@yale.edu>
2024-04-11 17:35:02 -0400

    linking grading.py and analysis.py
    also adjusted some settings values as needed, and removed CLI argparser, instead opting to change in code

--- api/grading.py
+++ api/grading.py
@@ -2,41 +2,56 @@ from sys import exit, stderr
 import os.path
 import argparse
 from settings import BASE_GRADE
-# from preprocessing import preliminary_pronunciation_check
-from analysis import get_sound_info
+from preprocessing import preliminary_pronunciation_check
+from analysis import grade_pitch_pattern
 
 # currently intended to be used in the command line while in development.
-def init_parser():
-    parser = argparse.ArgumentParser(allow_abbrev=False,
-                                     description='sound analysis')
-    parser.add_argument('filename', nargs='?', help='the filename of the soundfile to analyze', type=str)
-    parser.add_argument('expected_text', nargs='?', help='the word being said in the soundfile', type=str)
-    return parser
+# def init_parser():
+#     parser = argparse.ArgumentParser(allow_abbrev=False,
+#                                      description='sound analysis')
+#     parser.add_argument('filename', nargs='?', help='the filename of the soundfile to analyze', type=str)
+#     parser.add_argument('accent_type', nargs='?', help='accent type of the word being said. one of 0, 1, 2, 3, or 4', type=int)
+#     parser.add_argument('expected_text', nargs='?', help='the word being said in the soundfile', type=str)
+#     return parser
 
 def main():
     # current CLI interface lets us test individual sound files as well as the hard-coded example
-    parser = init_parser()
-    args = parser.parse_args()
-    filename = args.filename
-    expected_text = args.expected_text
+    # parser = init_parser()
+    # args = parser.parse_args()
+    # filename = args.filename
+    # accent_type = args.accent_type
+    # expected_text = args.expected_text
 
-    if filename is None or expected_text is None:
+    full_soundclip = ""
+    soundfile_array = []
+    accent_type = 0
+    expected_text = ""
+
+
+    if full_soundclip is None or expected_text is None:
         print("Incorrect parameters")
         return
 
-    print(f"Currently reading: {filename}")
-    if not os.path.isfile(filename):
+    print(f"Currently reading: {full_soundclip}")
+    if not os.path.isfile(full_soundclip):
         print("File not found")
         return
-    else:
-        grade = 0
-        # coefficient = preliminary_pronunciation_check(filename, expected_text)
-        coefficient = 1
-        print(f"Coefficient = {coefficient}")
-        if coefficient != 0: # if it is worth it to grade the sound file
-            # start with a base value that will be weighted according to the coefficient found.
-            grade += BASE_GRADE
-            get_sound_info(filename, expected_text)
+
+    for soundfile in soundfile_array:
+        if not os.path.isfile(soundfile):
+            print("File not found")
+            return
+
+    grade = 0
+    coefficient = preliminary_pronunciation_check(full_soundclip, expected_text)
+    # coefficient = 1 # for testing purposes to bypass coeff calculations
+    print(f"Coefficient = {coefficient}")
+    if coefficient != 0: # if it is worth it to grade the sound file
+        # start with a base value that will be weighted according to the coefficient found.
+        grade += BASE_GRADE
+        grade += (100 - BASE_GRADE) * grade_pitch_pattern(soundfiles=soundfile_array, accent_type=accent_type, word=expected_text)
+
+    return coefficient * grade
 
 if __name__ == "__main__":
     main()
--- api/settings.py
+++ api/settings.py
@@ -4,7 +4,7 @@
 SELECTED_MODEL = "base" # model type used for whisper. one of "tiny", "base", "small", "medium", and "large".
 
 # ~~~~~~~~~~~ PARAMETERS THAT AFFECT THE GRADE CALCULATION ~~~~~~~~~~~
-BASE_GRADE = 70 # the starting point for a non-zero coefficient grade
+BASE_GRADE = 55 # the starting point for a non-zero coefficient grade
 CORRECT_LANGUAGE_WEIGHT = 0.6 # weight given to an answer that gets the correct language detected.
 CORRECT_TEXT_WEIGHT = 0.4 # weight given to an answer that gets the correct input text detected.
 HIRAGANA_NOT_FOUND_PENALTY = 0.9 # penalty coefficient to which a grade should be multiplied if an expected hiragana is not found.
@@ -17,3 +17,4 @@ HOP_LENGTH = 512 # values indicate spacing between consecutive analysis windows
 FMIN = 40 # lower bound for frequency sampling
 FMAX = 1000 # upper bound for frequency sampling
 PITCH_TOLERANCE = 0.1 # indicates how close a pitch must be to its expected value. ie. 0.1 means it must be +/- 10% of the expected value.
+MINIMUM_DELTA = 1.5 # minimum expected change of pitch, in midi.
