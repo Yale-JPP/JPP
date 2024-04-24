3e1fc185f1c13a37f7429aee980b8eaa64202c43
PerryThePear <91431688+PerryThePear@users.noreply.github.com>
2024-04-23 23:04:34 -0400

    Small changes
    Changed to align with previous version of gaussian parsing. Also removed unnecessary code and add comments.

--- api/gaussian_parse.py
+++ api/gaussian_parse.py
@@ -1,6 +1,3 @@
-import os
-import math
-
 import librosa
 import librosa.display
 import soundfile as sf
@@ -12,47 +9,26 @@ import matplotlib.gridspec as gridspec
 from matplotlib.ticker import FormatStrFormatter
 from utilities import vowels, skip, data
 
-# I have the word -> I have the # of mora -> dipthong syllables count as one -> do below step
-# Gaussian -> # of gaussian is # of mora -> find peaks and get the lowest point in between to get timestamps
-# Separate dipthong later
-
-# Problem with 自由, maybe look at points of inflection as well?
-# 試合、試験
-# Doesn't perform well when vowels are next to each other, thus shi-a-i is bad, three vowels next to each other
-
-# Problem with the way we split - maybe have someway to make the vowels the same length, but make the portion of the consonant longer
-# Need to see if this happens often
-
-# Find a way to handle っ. Maybe if there is a plateau for a while, then split properly
-
-# What I need, basivally make a page with words, with the words have the associated mora and furigana so that when 
-# We look at the word, we can determine the mora and furigana for the gaussian
-
 vowels = ['あ', 'い', 'う', 'え', 'お', 'ん']
 skip = ['ゃ', 'ゅ', 'ょ']
 
 
 class GaussianParse():
     """
-    Takes the directory and file name of an audio file.
-    Makes the waveform of the audio file in order to find the
-    Guassian filtered curve, the peaks of that curve, and the 
-    dips of that curve. Can separate the audio file into syllables and 
-    plot the graph. 
+    Takes the file of an audio file and makes a waveform.
+    Guassian filters the curve to find the peaks and dips of that curve.
+    Can separate the audio file into syllables and plot the graph. 
     """
     # def __init__(self, dir, file, furigana, mora):
     def __init__(self, file, furigana, mora):    
         # Create the path and find the word
-        self._path = file
-        self._kanji = file[-6:-4]
-        
-        # --------FOR TESTING PURPOSES---------------
         self._furigana = furigana
         self._mora = mora
-        # -------------------------------------------
         
         # Load the waveform
-        self._original, self._sampling_rate = librosa.load(self._path)
+        self._original, self._sampling_rate = librosa.load(file)
+        
+        # Voice Isolate
         S_full, phase = librosa.magphase(librosa.stft(self._original))
         S_filter = librosa.decompose.nn_filter(S_full,
                                        aggregate=np.median,
@@ -68,8 +44,11 @@ class GaussianParse():
         S_foreground = mask_v * S_full
         new_y = librosa.istft(S_foreground*phase)
 
+        # Trim the silence from the beginning and end
         self._trimmed, self._index = librosa.effects.trim(new_y, top_db=40)
         self._original = self._original[self._index[0]: self._index[1]]
+
+        # Alter the wave_form's data
         self._waveform = np.array(self._trimmed, copy=True)
         self._waveform[self._waveform < 0] = 0
         
@@ -82,6 +61,7 @@ class GaussianParse():
         max = np.max(self._gauss_filt)
         self._gauss_filt /= max
 
+        # Get the number of double vowels
         num_of_double_vowels = 0
         for i in range(len(furigana)):
             if i == 0:
@@ -90,7 +70,10 @@ class GaussianParse():
                 if furigana[i] in vowels:
                     num_of_double_vowels += 1
 
-
+        # The expected peaks we get are on for each mora minus the devoiced vowels and double vowels
+        # We subtract one for /desu/ and by the number of souble vowels
+        # Then we repeatedly try lower peak_heights until we get the right amount of peaks
+        # There is the issue of getting more peaks than we want
         peak_height = .005
         self._peaks, _ = scipy.signal.find_peaks(self._gauss_filt, height=(peak_height / max))
         while len(self._peaks) < (mora - 1 - num_of_double_vowels):
@@ -104,19 +87,18 @@ class GaussianParse():
         for i in range(len(self._peaks) - 1):
             self._dips.append(np.where(self._gauss_filt == min(self._gauss_filt[self._peaks[i]:self._peaks[i+1]]))[0][0])
         self._dips = np.array(self._dips)
-
         self._splice_audio()
 
     def _splice_audio(self):
         """
-        Splices and separates the audio into syllables and outputs them in a new file.
+        Splices and separates the audio into syllables
         """
+        # Not all syllables in the word has been cut
         if len(self._dips) + 1 < self._mora - 1:
-            # Not all syllables in the word has been cut
             i = 0
             dip_indexer = 0
             while i < len(self._furigana[:-2]):
-                # If the moji combines with the previous moji to make one mora
+                # If the moji is small /ya/, /yu/, /yo/
                 if self._furigana[i] in skip:
                     i += 1
                     continue
@@ -139,24 +121,26 @@ class GaussianParse():
                 i += 1
                 dip_indexer += 1
         
-                
+        # Every syllable besides desu has been cut
         if len(self._dips) + 1 == self._mora - 1:
-            # Every syllable besides です has been cut
             desu = self._dips[-1]
             half_point = desu + ((self._waveform.size - desu) // 2)
             self._dips = np.append(self._dips, [half_point])
 
     def parse_clips(self):
+        """
+        Returns the clips of audio that were split.
+        """
         clips = []
         t1 = 0
         for i, end_timestamp in enumerate(self._dips):
-            # export_filename = "output/" + self._kanji + "_gp" + str(i) + ".wav"
+            # export_filename = "output/" + self._furigana + "_gp" + str(i) + ".wav"
             newAudio = self._original[t1:end_timestamp]
             clips.append(newAudio)
             # sf.write(export_filename, newAudio, self._sampling_rate)
             t1 = end_timestamp
         
-        # export_filename = "output/" + self._kanji + "_gp" + str(self._mora - 1) + ".wav"
+        # export_filename = "output/" + self._furigana + "_gp" + str(self._mora - 1) + ".wav"
         newAudio = self._original[t1:]
         clips.append(newAudio)
         # sf.write(export_filename, newAudio, self._sampling_rate)
@@ -164,7 +148,7 @@ class GaussianParse():
     
     def plot_waves(self):
         """
-        Plots all the waves and graphs. 
+        Plots all the waves and graphs. For research purposes. 
         """
         gs = gridspec.GridSpec(2, 2)
 
@@ -194,33 +178,12 @@ class GaussianParse():
         # plt.show()
 
     def get_original_clip_timestamps(self):
-        """Returns list of timestamps of when each mora starts, and when the last one ends"""
+        """
+        Returns list of timestamps of when each mora starts, and when the last one ends.
+        For research purposes. 
+        """
         res = [float(self._index[0]) / self._sampling_rate]
         for idx in self._dips:
             res.append(res[0] + (float(idx) / self._sampling_rate))
         res.append(float(self._index[1]) / self._sampling_rate)
         return res
-            
-if __name__ == "__main__":
-
-    gp = GaussianParse('1+2 Noun/家族.wav', "かぞくです", 5)
-    # gp.parse_clips()
-    # print(gp.get_original_clip_timestamps())
-    gp.plot_waves()
-
-    # original, sampling_rate = librosa.load('1+2 Noun/仕事.wav')
-    # times = [0.316, 0.4507265306122449, 0.5854530612244898, 0.7201795918367347, 0.8549061224489797, 0.9896326530612246]
-    # fig = plt.figure()
-    # librosa.display.waveshow(original, sr=sampling_rate, label='waveform')
-    # plt.plot(gp._time, gp._gauss_filt, label='gaussian filter')
-    # plt.plot(gp._time[gp._peaks], gp._gauss_filt[gp._peaks], "x", label='peaks')
-    # plt.plot(gp._time[gp._dips], gp._gauss_filt[gp._dips], "x", label='dips')
-    # # for x in times:
-    # plt.vlines(x = times, ymin = -.15, ymax = .15,
-    #     colors = 'purple',
-    #     ls='--',
-    #     label = 'duration parse')
-    # plt.legend()
-    # plt.xlabel("Time")
-    # plt.ylabel("Amplitude")
-    # plt.show()
